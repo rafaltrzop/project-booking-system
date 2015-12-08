e8e13611313f23bd79c82f056d0178320a9d42a0
Rafal Trzop <rafaltrzop@gmail.com>
2015-12-08 14:29:25 +0100

    Initial commit

---
+++ create.php
@@ -0,0 +1,190 @@
+<?php include('connection.php'); ?>
+
+<!-- DODAWANIE STUDENTA -->
+<?php
+
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email = $_POST['email'];
+$group = $_POST['group'];
+
+?>
+
+<h1>Dodaj nowego studenta</h1>
+<form action="" method="post">
+  <fieldset>
+    <legend>Dodaj studenta</legend>
+    <label>
+      Imię:<br>
+      <input type="text" name="first_name" placeholder="Jan" value="<?= $first_name ?>">
+    </label>
+
+    <br>
+    <br>
+
+    <label>
+      Nazwisko:<br>
+      <input type="text" name="last_name" placeholder="Kowalski" value="<?= $last_name ?>">
+    </label>
+
+    <br>
+    <br>
+
+    <label>
+      E-mail:<br>
+      <input type="email" name="email" placeholder="email@domena.pl" value="<?= $email ?>">
+    </label>
+
+    <br>
+    <br>
+
+    <label>
+      Grupa:<br>
+      <select name="group">
+        <option <?php if (empty($group)) echo 'selected' ?> disabled>&mdash;</option>
+        <option value="1" <?php if ($group == 1) echo 'selected' ?>>1</option>
+        <option value="2" <?php if ($group == 2) echo 'selected' ?>>2</option>
+        <option value="3" <?php if ($group == 3) echo 'selected' ?>>3</option>
+      </select>
+    </label>
+  </fieldset>
+
+  <input type="submit" value="Dodaj studenta">
+</form>
+
+<?php
+
+if (empty($first_name) || empty($last_name) || empty($email) || empty($group)) {
+  echo 'Błąd! Zmienna jest pusta.';
+} else {
+  mysql_query("INSERT INTO Osoba(email, imie, nazwisko) VALUES('$email', '$first_name', '$last_name')") or die('Błąd w zapytaniu');
+  $id_osoby = mysql_insert_id();
+
+  mysql_query("INSERT INTO Student(id_osoby, grupa) VALUES($id_osoby, $group)") or die('Błąd w zapytaniu');
+  echo "Dodano studenta: $first_name $last_name";
+}
+
+?>
+
+<!-- DODAWANIE PROFESORA -->
+<?php
+
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email = $_POST['email'];
+$school_subject = $_POST['school_subject'];
+
+?>
+
+<h1>Dodaj nowego profesora</h1>
+<form action="" method="post">
+  <fieldset>
+    <legend>Dodaj profesora</legend>
+    <label>
+      Imię:<br>
+      <input type="text" name="first_name" placeholder="Jan" value="<?= $first_name ?>">
+    </label>
+
+    <br>
+    <br>
+
+    <label>
+      Nazwisko:<br>
+      <input type="text" name="last_name" placeholder="Kowalski" value="<?= $last_name ?>">
+    </label>
+
+    <br>
+    <br>
+
+    <label>
+      E-mail:<br>
+      <input type="email" name="email" placeholder="email@domena.pl" value="<?= $email ?>">
+    </label>
+
+    <br>
+    <br>
+
+    <label>
+      Wykładany przedmiot:<br>
+      <input type="text" name="school_subject" value="<?= $school_subject ?>">
+    </label>
+  </fieldset>
+
+  <input type="submit" value="Dodaj profesora">
+</form>
+
+<?php
+
+if (empty($first_name) || empty($last_name) || empty($email) || empty($school_subject)) {
+  echo 'Błąd! Zmienna jest pusta.';
+} else {
+  mysql_query("INSERT INTO Osoba(email, imie, nazwisko) VALUES('$email', '$first_name', '$last_name')") or die('Błąd w zapytaniu');
+  $id_osoby = mysql_insert_id();
+  mysql_query("INSERT INTO Profesor VALUES($id_osoby, '$school_subject')") or die('Błąd w zapytaniu');
+  echo "Dodano profesora: $first_name $last_name";
+}
+
+?>
+
+<!-- DODAWANIE PROJEKTU -->
+<h1>Dodaj nowy temat projektu</h1>
+<form action="" method="post">
+  <label>
+    Temat projektu:<br>
+    <input type="text" name="project_name" required>
+  </label>
+  <input type="submit" value="Dodaj projekt">
+</form>
+
+<?php
+
+$project_name = $_POST['project_name'];
+
+if (empty($project_name)) {
+  echo 'Błąd! Zmienna jest pusta.';
+} else {
+  mysql_query("INSERT INTO Projekt(temat) VALUES('$project_name')") or die('Błąd w zapytaniu');
+  echo "Dodano projekt: $project_name";
+}
+
+?>
+
+<!-- ZGŁASZANIE WYKONANIA PROJEKTU -->
+<?php
+
+$id_student = $_POST['id_student'];
+$submission_date = $_POST['submission_date'];
+
+?>
+
+<h1>Zgłoś wykonanie projektu</h1>
+<form action="" method="post" onsubmit="return confirm('Czy aby na pewno chcesz zgłosić swój projekt do oceny?');">
+  <label>
+    Wybierz swoje nazwisko:<br>
+    <select name="id_student">
+      <option selected disabled>&mdash;</option>
+      <?php
+
+      $result = mysql_query("SELECT id_osoby, imie, nazwisko FROM Osoba NATURAL JOIN Student") or die('Błąd w zapytaniu');
+      while ($row = mysql_fetch_array($result)) {
+        echo '<option value="'.$row[0].'">'.$row[1].' '.$row[2].'</option>';
+      }
+
+      ?>
+    </select>
+  </label>
+
+  <input type="hidden" name="submission_date" value="<?= date('Y-m-d') ?>">
+  <input type="submit" value="Zgłoś projekt do oceny">
+</form>
+
+<?php
+
+if (empty($id_student) || empty($submission_date)) {
+  echo 'Błąd! Zmienna jest pusta.';
+} else {
+  mysql_query("INSERT INTO Wykonany_projekt(id_osoby_student, data_oddania) VALUES('$id_student', '$submission_date')") or die('Błąd w zapytaniu');
+  echo "Twój projekt został oznaczony jako wykonany i zgłoszony do oceny.";
+}
+
+?>
---
+++ read.php
@@ -0,0 +1,100 @@
+<?php
+
+include('connection.php');
+
+# Tabela Osoba
+echo '<h1>Osoba</h1>
+      <table border="1">
+        <tr>
+          <th>id_osoby</th>
+          <th>email</th>
+          <th>imie</th>
+          <th>nazwisko</th>
+        </tr>';
+
+$result = mysql_query('SELECT * FROM Osoba') or die('Błąd w zapytaniu');
+while ($row = mysql_fetch_array($result)) {
+  echo "<tr>
+          <td>$row[0]</td>
+          <td>$row[1]</td>
+          <td>$row[2]</td>
+          <td>$row[3]</td>
+        </tr>";
+}
+echo '</table>';
+
+# Tabela Student
+echo '<h1>Student</h1>
+      <table border="1">
+        <tr>
+          <th>id_osoby</th>
+          <th>grupa</th>
+          <th>nr_projektu</th>
+        </tr>';
+
+$result = mysql_query('SELECT * FROM Student') or die('Błąd w zapytaniu');
+while ($row = mysql_fetch_array($result)) {
+  echo "<tr>
+          <td>$row[0]</td>
+          <td>$row[1]</td>
+          <td>$row[2]</td>
+        </tr>";
+}
+echo '</table>';
+
+# Tabela Profesor
+echo '<h1>Profesor</h1>
+      <table border="1">
+        <tr>
+          <th>id_osoby</th>
+          <th>wykladany_przedmiot</th>
+        </tr>';
+
+$result = mysql_query('SELECT * FROM Profesor') or die('Błąd w zapytaniu');
+while ($row = mysql_fetch_array($result)) {
+  echo "<tr>
+          <td>$row[0]</td>
+          <td>$row[1]</td>
+        </tr>";
+}
+echo '</table>';
+
+# Tabela Projekt
+echo '<h1>Projekt</h1>
+      <table border="1">
+        <tr>
+          <th>nr_projektu</th>
+          <th>temat</th>
+        </tr>';
+
+$result = mysql_query('SELECT * FROM Projekt') or die('Błąd w zapytaniu');
+while ($row = mysql_fetch_array($result)) {
+  echo "<tr>
+          <td>$row[0]</td>
+          <td>$row[1]</td>
+        </tr>";
+}
+echo '</table>';
+
+# Tabela Wykonany_projekt
+echo '<h1>Wykonany_projekt</h1>
+      <table border="1">
+        <tr>
+          <th>id_osoby_student</th>
+          <th>id_osoby_profesor</th>
+          <th>data_oddania</th>
+          <th>ocena</th>
+        </tr>';
+
+$result = mysql_query('SELECT * FROM Wykonany_projekt') or die('Błąd w zapytaniu');
+while ($row = mysql_fetch_array($result)) {
+  echo "<tr>
+          <td>$row[0]</td>
+          <td>$row[1]</td>
+          <td>$row[2]</td>
+          <td>$row[3]</td>
+        </tr>";
+}
+echo '</table>';
+
+?>
