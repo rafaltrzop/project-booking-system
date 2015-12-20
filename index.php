fc371726873e316d69272a1915a6906b12387059
Rafal Trzop <rafaltrzop@gmail.com>
2015-12-20 15:59:33 +0100

    Add partials and simple index.php page

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include('partials/head.html'); ?>
+
+<div class="row">
+  <div class="large-12 columns">
+    <h1>System rezerwacji tematów projektów</h1>
+  </div>
+</div>
+
+<?php include('partials/scripts.html'); ?>
