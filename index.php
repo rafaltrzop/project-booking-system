cdc5f10d500ae7906dec6b05c7c088f1fc1dc399
Rafal Trzop <rafaltrzop@gmail.com>
2015-12-21 21:04:43 +0100

    Add control panel for students

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php include('partials/head.html'); ?>
 
-<div class="row">
-  <div class="small-12 columns text-center absolute-center">
-    <span id="landing-page-logo" class="fa fa-graduation-cap"></span>
-    <div class="is-animating">
-      <h1 id="landing-page-text">System&nbsp;rezerwacji tematów&nbsp;projektów</h1>
-      <a href="#" class="large button btn-landing-page">Panel studenta</a>
-      <a href="#" class="large button btn-landing-page">Panel profesora</a>
+  <body id="landing-page">
+    <div class="row">
+      <div class="small-12 columns text-center absolute-center">
+        <span id="landing-page-logo" class="fa fa-graduation-cap"></span>
+        <div class="is-animating">
+          <h1 id="landing-page-text">System&nbsp;rezerwacji tematów&nbsp;projektów</h1>
+          <a href="/students" class="large button btn-landing-page">Panel studenta</a>
+          <a href="#" class="large button btn-landing-page">Panel profesora</a>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
 
 <?php include('partials/scripts.html'); ?>
