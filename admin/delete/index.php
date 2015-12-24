59ae81c008d4870deb8e75b710b59c25b9a36220
Rafal Trzop <rafaltrzop@gmail.com>
2015-12-24 14:02:53 +0100

    Add forms for deleting things from database

--- admin/delete/index.php
+++ admin/delete/index.php
@@ -9,7 +9,133 @@ include($_SERVER['DOCUMENT_ROOT'].'/partials/header_admin.html');
 <main class="body-content-container">
   <div class="row">
     <div class="small-12 columns">
-      <h1>Usuń</h1>
+      <!-- USUWANIE PROJEKTU -->
+      <?php
+
+      $project_removed = FALSE;
+      $project_number = trim($_POST['project_number']);
+
+      if (!empty($project_number)) {
+        // wykonuję zapytanie przed wyświetleniem listy tematów aby była ona aktualna po wysłaniu formularza
+        // (nie chcę aby na liście projektów znowu pojawił się temat który dopiero co usunąłem)
+        mysql_query("DELETE FROM Projekt WHERE nr_projektu = $project_number") or die(mysql_error());
+        $project_removed = TRUE;
+      }
+
+      ?>
+
+      <h1>Usuń temat projektu</h1>
+      <form action="" method="post">
+        <label>
+          <ol>
+            <?php
+
+            $result = mysql_query('SELECT * FROM Projekt ORDER BY temat') or die(mysql_error());
+            while ($row = mysql_fetch_array($result)) {
+              echo '<li>
+                      <label>
+                        <input type="radio" name="project_number" value="'.$row[0].'" required>
+                        '.$row[1].'
+                      </label>
+                    </li>';
+            }
+
+            ?>
+          </ol>
+        </label>
+        <?php
+
+        if ($project_removed) {
+          echo '<p><span class="fa fa-check fa-success"></span>&ensp;Wybrany temat projektu został usunięty.</p>';
+        }
+
+        ?>
+        <button type="submit" class="hollow button">Usuń wybrany temat</button>
+      </form>
+
+      <!-- USUWANIE STUDENTA -->
+      <?php
+
+      $id_student = trim($_POST['id_student']);
+
+      if (!empty($id_student)) {
+        mysql_query("DELETE FROM Osoba WHERE id_osoby = $id_student") or die(mysql_error());
+      }
+
+      ?>
+
+      <h1>Usuń studenta</h1>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Imię i nazwisko</th>
+            <th>E-mail</th>
+            <th>Grupa</th>
+            <th>Opcje</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+
+          $result = mysql_query('SELECT imie, nazwisko, email, grupa, id_osoby FROM Student NATURAL JOIN Osoba ORDER BY imie, nazwisko') or die(mysql_error());
+          while ($row = mysql_fetch_array($result)) {
+            echo '<tr>
+                    <td>'.$row[0].' '.$row[1].'</td>
+                    <td>'.$row[2].'</td>
+                    <td>'.$row[3].'</td>
+                    <td>
+                      <form action="" method="post">
+                        <input type="hidden" name="id_student" value="'.$row[4].'">
+                        <button type="submit" class="hollow button">Usuń studenta</button>
+                      </form>
+                    </td>
+                  </tr>';
+          }
+          ?>
+        </tbody>
+      </table>
+
+      <!-- USUWANIE PROFESORA -->
+      <?php
+
+      $id_proffesor = trim($_POST['id_proffesor']);
+
+      if (!empty($id_proffesor)) {
+        mysql_query("DELETE FROM Osoba WHERE id_osoby = $id_proffesor") or die(mysql_error());
+      }
+
+      ?>
+
+      <h1>Usuń profesora</h1>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Imię i nazwisko</th>
+            <th>E-mail</th>
+            <th>Wykładany przedmiot</th>
+            <th>Opcje</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+
+          $result = mysql_query('SELECT imie, nazwisko, email, wykladany_przedmiot, id_osoby FROM Profesor NATURAL JOIN Osoba ORDER BY imie, nazwisko') or die(mysql_error());
+          while ($row = mysql_fetch_array($result)) {
+            echo '<tr>
+                    <td>'.$row[0].' '.$row[1].'</td>
+                    <td>'.$row[2].'</td>
+                    <td>'.$row[3].'</td>
+                    <td>
+                      <form action="" method="post">
+                        <input type="hidden" name="id_proffesor" value="'.$row[4].'">
+                        <button type="submit" class="hollow button">Usuń profesora</button>
+                      </form>
+                    </td>
+                  </tr>';
+          }
+          ?>
+        </tbody>
+      </table>
     </div>
   </div>
 </main>
