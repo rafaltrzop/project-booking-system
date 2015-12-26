c376cf1f5f092a6f17d304c9ea951cfaa25053b8
Rafal Trzop <rafaltrzop@gmail.com>
2015-12-26 12:22:09 +0100

    Add form for editing project names

--- admin/edit/index.php
+++ admin/edit/index.php
@@ -9,7 +9,41 @@ include($_SERVER['DOCUMENT_ROOT'].'/partials/header_admin.html');
 <main class="body-content-container">
   <div class="row">
     <div class="small-12 columns">
-      <h1>Edytuj</h1>
+      <h1>Edytuj temat projektu</h1>
+      <?php
+
+      $project_number = trim($_POST['project_number']);
+      $project_name = trim($_POST['project_name']);
+
+      if (!empty($project_number) && !empty($project_name)) {
+        mysql_query("UPDATE Projekt SET temat = '$project_name' WHERE nr_projektu = $project_number") or die(mysql_error());
+        echo '<p><span class="fa fa-check fa-success"></span>&ensp;Zmieniono nazwę tematu projektu na: '.$project_name.'.</p>';
+      }
+
+      ?>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Temat projektu</th>
+            <th class="text-center">Opcje</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+
+          $result = mysql_query('SELECT * FROM Projekt ORDER BY temat') or die(mysql_error());
+          while ($row = mysql_fetch_array($result)) {
+            echo '<tr>
+                    <td>'.$row[1].'</td>
+                    <td>
+                      <a href="/admin/edit/project.php?id='.$row[0].'" class="hollow button">Edytuj temat</a>
+                    </td>
+                  </tr>';
+          }
+
+          ?>
+        </tbody>
+      </table>
     </div>
   </div>
 </main>
---
+++ admin/edit/project.php
@@ -0,0 +1,33 @@
+<?php
+
+include($_SERVER['DOCUMENT_ROOT'].'/connection.php');
+include($_SERVER['DOCUMENT_ROOT'].'/partials/head.php');
+include($_SERVER['DOCUMENT_ROOT'].'/partials/header_admin.html');
+
+?>
+
+<main class="body-content-container">
+  <div class="column row">
+    <?php
+
+    $project_number = (int) trim($_GET['id']);
+
+    $result = mysql_query("SELECT temat FROM Projekt WHERE nr_projektu = $project_number") or die(mysql_error());
+    $row = mysql_fetch_array($result);
+
+    ?>
+    <h1>Edycja tematu projektu</h1>
+    <form action="/admin/edit/index.php" method="post">
+      <label>
+        Temat projektu:
+        <input type="hidden" name="project_number" value="<?= $project_number ?>" required>
+        <input type="text" name="project_name" value="<?= $row[0] ?>" required>
+      </label>
+
+      <button type="submit" class="hollow button">Zapisz zmiany</button>
+      <button type="button" class="hollow button" onclick="history.back();">Anuluj</button>
+    </form>
+  </div>
+</main>
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/partials/scripts.html'); ?>
