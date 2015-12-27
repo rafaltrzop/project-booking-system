1b4ac441095e6876316c83ffdafda04bf9c030e7
Rafal Trzop <rafaltrzop@gmail.com>
2015-12-27 12:50:34 +0100

    Add favicon

--- partials/head.php
+++ partials/head.php
@@ -6,5 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>System rezerwacji tematów projektów</title>
     <link rel="stylesheet" href="/css/app.css">
+    <link rel="icon" href="/favicon.ico">
   </head>
   <body<?php if (!empty($body_class)) echo ' class="'.$body_class.'"' ?>>
